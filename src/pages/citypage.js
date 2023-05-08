755e1443c3cf2381b0045703b974bed4530af8f5
Monika Gardyjas <gardyjas.monika1@gmail.com>
2023-05-08 19:53:53 +0200

    setting responsive background image on citypage

--- src/pages/citypage.js
+++ src/pages/citypage.js
@@ -4,81 +4,48 @@ import Button from '@mui/material/Button';
 import React, { useState } from 'react';
 import Header from '../components/header';
 import StatefulCityModal from '../components/StatefulCityModal/StatefulCityModal';
+import backgroundImage from '../citypage-background-1ver.png';
 
 function CityPage() {
     const [show, setShow] = useState(false) // czy potrzebne ? 
 
     return (
-        <div className="App">
+        // <div className="App">
+        <div className='container'>
             <Header />
-            <h1>
-                CityPage
-            </h1>
-            
-            <div className='fotowoltaika'>
-                <StatefulCityModal title="fotowoltaika" buttonText="panele fotowoltaiczne">
-                    <p> Dzień dobry,
-                        czy są Państwo zainteresowani instalacją paneli fotowoltaiczych?
-                    </p>
-                </StatefulCityModal>
-            </div>
+            <div className="background-image" style={{ backgroundImage: `url(${backgroundImage})` }}> 
+                    <h1>
+                        CityPage
+                    </h1>
 
-            <div className='transport'>
-                <StatefulCityModal title="transport" buttonText="pociąg">
-                    <p>
-                        Przedstawimy teraz PLAN NA NAPRAWĘ POLSKIEJ KOLEI
-                    </p>
-                </StatefulCityModal>
-            </div>
+                    <div className='fotowoltaika'>
+                        <StatefulCityModal title="fotowoltaika" buttonText="panele fotowoltaiczne">
+                            <p> 
+                                Dzień dobry,
+                                czy są Państwo zainteresowani instalacją paneli fotowoltaiczych?
+                            </p>
+                        </StatefulCityModal>
+                    </div>
 
-            <div className='dron'>
-                <StatefulCityModal title="dron" buttonText="dron">
-                    <p>
-                        PTAKI VS DRONY 
-                    </p>
-                </StatefulCityModal>
-            </div>
+                    <div className='transport'>
+                        <StatefulCityModal title="transport" buttonText="pociąg">
+                            <p>
+                                Przedstawimy teraz PLAN NA NAPRAWĘ POLSKIEJ KOLEI
+                            </p>
+                        </StatefulCityModal>
+                    </div>
 
+                    <div className='dron'>
+                        <StatefulCityModal title="dron" buttonText="dron">
+                            <p>
+                                PTAKI VS DRONY 
+                            </p>
+                        </StatefulCityModal>
+                    </div>
+                </div>
         </div>
+        // </div>
     ); 
 }
 
 export default CityPage;
-
-// const openModalButtons = document.querySelectorAll('[data-modal-target]')
-// const closeModalButtons = document.querySelectorAll('[data-close-button]')
-// const overlay = document.getElementById('overlay')
-
-// openModalButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         const modal = document.querySelector(button.dataset.modalTarget)
-//         openModal(modal)
-//     })
-// })
-
-// closeModalButtons.forEach(button => {
-//     button.addEventListener('click', () => {
-//         const modal = button.closest('.modal')
-//         closeModal(modal)
-//     })
-// })
-
-// overlay.addEventListener('click', () => {
-//     const modals = document.querySelectorAll('.modal.active')
-//     modals.forEach(modal => {
-//         closeModal(modal)
-//     })
-// })
-
-// function openModal(modal) {
-//     if (modal == null) return
-//     modal.classList.add('active')
-//     overlay.classList.add('active')
-// }
-
-// function closeModal(modal) {
-//     if (modal == null) return
-//     modal.classList.remove('active')
-//     overlay.classList.remove('active')
-// }
-
